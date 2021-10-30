a31d4133b21b42941056678bb1912c2dbe2c0d01
yourcontactformsubmitted <93413371+yourcontactformsubmitted@users.noreply.github.com>
2021-10-30 06:44:52 +0530

    initial commit

---
+++ dxfkdjk.js
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Web3Demo</title>
+  </head>
+  <body>
+    <span id="result"> </span>
+
+    <form id="form">
+      <table>
+        <tr>
+          <td>Title</td>
+          <td> <input type="text" id="name"></td>
+      </tr>
+      <tr>
+          <td>Author</td>
+          <td><input type="text" id="author"></td>
+      </tr>
+          </table>
+          <input type="submit">
+    </form>
+  </body>
+</html>
+
+/* <!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Web3Demo</title>
+  </head>
+  <body>
+    <span id="result"> </span>
+
+    <form id="form">
+      <table>
+        <tr>
+          <td>Title</td>
+          <td> <input type="text" id="name"></td>
+      </tr>
+      <tr>
+          <td>Author</td>
+          <td><input type="text" id="author"></td>
+      </tr>
+          </table>
+          <input type="submit">
+    </form>
+  </body>
+</html> */
